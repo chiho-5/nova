fbee4c4ae7087f21a6e8fcb127961631a77bc666
Ezeora Nnenna Esther <71467633+chiho-5@users.noreply.github.com>
2025-01-08 08:03:47 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,3 @@
+pip install fastembed llama-index-embeddings-fastembed
+pip install  Pillow scikit-learn scipy uvicorn python-multipart 
+pip install llama-index llama-index-core llama-index-readers-web llama-index-llms-huggingface-api llama-index-llms-huggingface fastapi
